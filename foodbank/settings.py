ffa2e316aa14cc1901192743e23a577296cfd6a1
dumbastic <dumbastic@gmail.com>
2014-12-06 11:44:17 +0000

    setting static files

--- foodbank/settings.py
+++ foodbank/settings.py
@@ -82,7 +82,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
